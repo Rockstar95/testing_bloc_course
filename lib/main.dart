7cfee1cf6b62f0c335326e7af345dee954e8f26f
Dishant <agrawaldishant29@gmail.com>
2022-07-25 09:57:51 +0530

    Removed Unnecessary code from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:testing_bloc_course/app_bloc_observer.dart';
 import 'package:testing_bloc_course/names_cubit_example/ui/names_cubit_home_screen.dart';
-import 'package:testing_bloc_course/random_image_loader_example/ui/random_image_loader_home_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,53 +20,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Home Screen"),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              children: [
-                getButton("Names Cubit Screen", () {
-                  Navigator.push(context, MaterialPageRoute(builder: (_) => const NameCubitHomeScreen()));
-                }),
-                getButton("Random Image Loader Screen", () {
-                  Navigator.push(context, MaterialPageRoute(builder: (_) => const RandomImageLoaderHomeScreen()));
-                }),
-              ],
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget getButton(String text, void Function() onTap) {
-    return FlatButton(
-      onPressed: () {
-        onTap();
-      },
-      color: Colors.blue,
-      child: Text(text, style: const TextStyle(color: Colors.white),),
+      //home: RandomImageLoaderHomeScreen(),
+      home: NameCubitHomeScreen(),
     );
   }
 }
